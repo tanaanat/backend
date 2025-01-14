2b74996fc050a3c764e3561f1559613e11a758ba
tanaanat <takumadagu@gmail.com>
2025-01-14 15:38:56 +0900

    1/14 -3

--- migration/env.py
+++ migration/env.py
@@ -8,7 +8,7 @@ from backend.app.model.table import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("DB_URL", os.environ.get("DB_URL"))
+config.set_main_option("DATABASE_URL", os.environ.get("DATABASE_URL"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
